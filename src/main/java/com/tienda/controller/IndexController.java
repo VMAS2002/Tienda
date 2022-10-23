67cd0912fd8b9825107492afa80a056ca152c64d
VMAS2002 <103341408+VMAS2002@users.noreply.github.com>
2022-10-23 15:04:33 -0600

    Práctica 2 Terminada

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,36 @@
+package com.tienda.controller;
+
+import com.tienda.dao.ClienteDao;
+import com.tienda.domain.Cliente;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class IndexController {
+
+    @Autowired
+    private ClienteDao clienteDao;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        log.info("Estamos usando una arquitectura mvc");
+
+        Cliente cliente = new Cliente("Pedro", "Gomez Contreras", "pgomez@gmail.com", "5555-7878");
+        //Cliente cliente2=new Cliente ("Rebeca", "Arguedas Molina", "rarguedas@gmail.com", "7855-5578");
+
+        //var clientes=Arrays.asList(cliente1, cliente2);
+        var clientes = clienteDao.findAll();
+                
+        model.addAttribute("cliente", cliente);
+
+        model.addAttribute("clientes", clientes);
+
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -0,0 +1,33 @@
+package com.tienda.domain;
+
+import javax.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="cliente")
+public class Cliente {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Long idCliente;//Hibeernate lo convierte en id_cliente 
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String telefono;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String apellidos, String correo, String telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+    
+    
+}
