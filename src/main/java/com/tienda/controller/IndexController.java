20d1b3cd942c9773a85010e1b57dca2787a9e52f
VMAS2002 <103341408+VMAS2002@users.noreply.github.com>
2022-12-05 19:14:56 -0600

    Semana 8

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,7 +1,7 @@
 package com.tienda.controller;
 
-import com.tienda.domain.Cliente;
-import com.tienda.service.ClienteService;
+import com.tienda.domain.Articulo;
+import com.tienda.service.ArticuloService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,46 +14,14 @@ import org.springframework.web.bind.annotation.*;
 public class IndexController {
 
     @Autowired
-    private ClienteService clienteService;
+    private ArticuloService articuloService;
 
     @GetMapping("/")
     public String inicio(Model model) {
         log.info("Estamos usando una arquitectura mvc");
-
-        Cliente cliente = new Cliente("Pedro", "Gomez Contreras", "pgomez@gmail.com", "5555-7878");
-        //Cliente cliente2=new Cliente ("Rebeca", "Arguedas Molina", "rarguedas@gmail.com", "7855-5578");
-
-        //var clientes=Arrays.asList(cliente1, cliente2);
-        var clientes = clienteService.getClientes();
-
-        model.addAttribute("cliente", cliente);
-
-        model.addAttribute("clientes", clientes);
-
+        var articulos = articuloService.getArticulos(true);
+        model.addAttribute("articulos", articulos);
         return "index";
-    }
 
-    @GetMapping("/nuevoCliente")
-    public String nuevoCliente(Cliente cliente) {
-        return "modificarCliente";
-    }
-
-    @PostMapping("/guardarCliente")
-    public String guardarCliente(Cliente cliente) {
-        clienteService.save(cliente);
-        return "redirect:/";
-    }
-
-    @GetMapping("/modificarCliente/{idCliente}")
-    public String modificarCliente(Cliente cliente, Model model) {
-        cliente = clienteService.getCliente(cliente);
-        model.addAttribute("cliente", cliente);
-        return "modificarCliente";
-    }
-
-    @GetMapping("/eliminarCliente/{idCliente}")
-    public String eliminarCliente(Cliente cliente) {
-        clienteService.delete(cliente);
-        return "redirect:/";
     }
 }
---
+++ src/main/java/com/tienda/service/ArticuloServiceImpl.java
@@ -0,0 +1,46 @@
+package com.tienda.service;
+
+import com.tienda.dao.CategoriaDao;
+import com.tienda.domain.Categoria;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class ArticuloServiceImpl implements CategoriaService {
+    
+    @Autowired
+    CategoriaDao categoriaDao;
+
+    @Override
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = (List<Categoria>)categoriaDao.findAll();
+        
+        if (activos){
+            lista.removeIf(e -> e.isActivo());
+        }
+        
+        return lista;
+    }
+
+    @Override
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse (null);
+        
+    }
+
+    @Override
+    public void save(Categoria categoria) {
+      categoriaDao.save(categoria);
+    }
+
+    @Override
+    public void delete(Categoria categoria) {
+       categoriaDao.delete(categoria);
+    }
+
+
+
+}
